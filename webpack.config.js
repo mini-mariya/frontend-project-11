88445c1a4a1aebf697e6f3d59a8d6a6f2edae2aa
Мария Зырянова <m3305@mail.ru>
2022-10-29 19:31:41 +0300

    add MiniCssExtractPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
@@ -17,6 +19,10 @@ const config = {
     new HtmlWebpackPlugin({
       template: 'index.html',
     }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+    }),
+    new CleanWebpackPlugin(),
   ],
   module: {
     rules: [
