7253979638ba3f39927d210181a281c92fa8545e
Мария Зырянова <m3305@mail.ru>
2022-10-29 19:24:11 +0300

    change src/locales/index.js

--- src/locales/index.js
+++ src/locales/index.js
@@ -1,8 +1,3 @@
-export default {
-  translation: {
-    notValidUrl: 'Ссылка должна быть валидным URL',
-    notValidDouble: 'RSS уже существует',
-    notValidRss: 'Ресурс не содержит валидный RSS',
-    validRss: 'RSS успешно загружен',
-  },
-};
+import ru from './ru.js';
+
+export default { ru };
