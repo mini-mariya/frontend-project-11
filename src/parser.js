46ddd61150a657bf584f972f244818edc7d824d4
Мария Зырянова <m3305@mail.ru>
2022-10-29 19:25:15 +0300

    change src/parser.js

--- src/parser.js
+++ src/parser.js
@@ -1,18 +1,33 @@
-export default (contents, type = 'application/xml') => {
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(contents, type);
-  const error = doc.querySelector('parserError');
-  if (error) {
-    throw new Error(error.textContent);
-  }
-  const title = doc.querySelector('title').textContent;
-  const description = doc.querySelector('description').textContent;
-  const post = [...doc.querySelectorAll('item')];
-  const items = post.map((item) => {
-    const titlePost = item.querySelector('title').textContent;
-    const descriptionPost = item.querySelector('description').textContent;
+const getPosts = (data) => {
+  const items = data.querySelectorAll('channel > item');
+  const posts = [...items].map((item) => {
+    const title = item.querySelector('title').textContent;
+    const description = item.querySelector('description').textContent;
     const link = item.querySelector('link').textContent;
-    return { title: titlePost, description: descriptionPost, link };
+
+    return { title, description, link };
   });
-  return { title, description, items };
+  return posts;
+};
+
+const getFeed = (data) => {
+  const title = data.querySelector('channel > title').textContent;
+  const description = data.querySelector('channel > description').textContent;
+
+  return { title, description };
+};
+
+export default (rss) => {
+  const parser = new DOMParser();
+  const parsed = parser.parseFromString(rss, 'text/xml');
+  const parserError = parsed.querySelector('parsererror');
+
+  if (parserError) {
+    throw new Error(parserError);
+  }
+
+  const feed = getFeed(parsed);
+  const posts = getPosts(parsed);
+
+  return { feed, posts };
 };
